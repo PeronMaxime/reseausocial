32f0cc131d8edd8b2cd3f633d45260e3814e1f21
PeronMaxime <38103096+PeronMaxime@users.noreply.github.com>
2020-06-26 15:17:50 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const { v1: uuidv1 } = require('uuid');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-const port = process.env.Port || 1234;
+const port = process.env.Port || 5000;
 
 /****************************************************
  ********************Serveur Http********************
